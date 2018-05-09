feae1e4fd2b942cf8040a5b49d77eee28854af94
Matt Belisle <matthew.belisle@workiva.com>
2018-05-08 21:58:02 -0500

    Kotlin WIP

--- kotlin/Sudoku.kt
+++ kotlin/Sudoku.kt
@@ -1,24 +1,7 @@
-func buddies(i: Int): List<int> {
-
-}
-
-fun solve(puzzle: Array<Int>): Array<Int> {
-    return puzzle
+fun parse(file: String) {
+    println(file)
 }
 
 fun main(args: Array<String>) {
-    val array = arrayOf(
-            4, 9, 0,  2, 0, 1,  7, 0, 0,
-            1, 2, 3,  0, 0, 0,  0, 8, 0,
-            0, 0, 0,  0, 4, 0,  0, 2, 1,
-
-            0, 0, 0,  8, 0, 0,  3, 0, 7,
-            0, 0, 2,  4, 7, 9,  1, 0, 0,
-            9, 0, 8,  0, 0, 3,  0, 0, 0,
-
-            8, 3, 0,  0, 5, 0,  0, 0, 0,
-            0, 4, 0,  0, 0, 0,  8, 6, 5,
-            0, 0, 9,  6, 0, 8,  0, 1, 3
-    )
-    println(solve(array))
+    parse(args[0])
 }
