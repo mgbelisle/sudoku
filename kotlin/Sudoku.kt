4c106de61377bc6b485ac849aaf3928d25b5de9c
Matt Belisle <matthew.belisle@workiva.com>
2018-05-10 08:03:51 -0500

    Puzzle var done

--- kotlin/Sudoku.kt
+++ kotlin/Sudoku.kt
@@ -6,13 +6,6 @@ fun solve(puzzle: List<Set<Byte>>): List<Set<Byte>> {
 
 fun main(args: Array<String>) {
     val puzzle = File(args[0]).readText().trim().split(regex = "\\s+".toRegex())
-            .map { it.toByte() }
-            .map {
-                if (it == 0.toByte()) {
-                    (1..9).toSet()
-                } else {
-                    setOf(it)
-                }
-            }
+            .map { if (it == "0") (1..9).toSet() else setOf(it.toByte()) }
     println(puzzle)
 }
