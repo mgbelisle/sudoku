b4d38bef64c24f772dbe11d36c8e22756db507bf
Matt Belisle <matthew.belisle@workiva.com>
2018-05-10 23:12:18 -0500

    WIP

--- kotlin/Sudoku.kt
+++ kotlin/Sudoku.kt
@@ -1,11 +1,16 @@
 import java.io.File
 
-fun solve(puzzle: List<Set<Byte>>): List<Set<Byte>> {
-    return emptyList()
+fun buddies(i: Int): List<Int> {
+    return (0..80).filter {
+        i / 9 == it / 9
+        || i % 9 == it % 9
+    }
 }
 
 fun main(args: Array<String>) {
     val puzzle = File(args[0]).readText().trim().split(regex = "\\s+".toRegex())
             .map { if (it == "0") (1..9).toSet() else setOf(it.toByte()) }
-    println(puzzle)
+    for (i in (0..puzzle.size-1)) {
+        println(buddies(i))
+    }
 }
