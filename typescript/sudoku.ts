c83dd5271a74782122e8147281b929e5fe21bbbe
Matthew Belisle <matthew.belisle@workiva.com>
2024-07-28 22:32:59 -0500

    Figuring out match

--- typescript/sudoku.ts
+++ typescript/sudoku.ts
@@ -1,6 +1,9 @@
-let puzzle = require('fs').readFileSync(0, 'utf-8').match(/\d/g).map(Number);
-if (puzzle.length !== 81) {
-  console.log('Puzzle must be 81 digits long');
-  process.exit(1);
+function stringToPuzzle(s: string): number[] {
+    const puzzle = s.match(/\d/g).map(Number);
+    if (puzzle.length !== 81) {
+        process.exit('Puzzle must be 81 digits long');
+    }
+    return puzzle;
 }
-console.log(puzzle);
+
+const puzzle = stringToPuzzle(require('fs').readFileSync(0, 'utf-8'));
